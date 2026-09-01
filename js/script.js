e639b7c60864c77a4d3c3da64d93a6aec4684582
parsamaghboul <323445828+parsamaghboul@users.noreply.github.com>
2026-09-01 09:41:50 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-window.onload = function () {
-    alert("⚠️ Important Notice: Welcome to the “Khan Daee Restaurant” website. Please note that this website is solely a web design and development portfolio project and does not belong to a real restaurant.");
-
-    alert("All information, images, trust badges, and other elements displayed on this website are for demonstration purposes only and are not real. Please take this notice into consideration before relying on or using any information provided on this website. Any use of the information or content on this website is at the user's own discretion and responsibility.");
-};
-    
 let topbtn = document.getElementById("topbtn");
 topbtn.addEventListener("click", function () {
     window.scrollTo({
@@ -16,6 +10,12 @@ let button = document.getElementById("dark");
 button.addEventListener("click", function () {
     document.body.classList.toggle("dark");
 });
+
+window.onload = function () {
+    alert("⚠️ Important Notice: Welcome to the “Khan Daee Restaurant” website. Please note that this website is solely a web design and development portfolio project and does not belong to a real restaurant.");
+
+    alert("All information, images, trust badges, and other elements displayed on this website are for demonstration purposes only and are not real. Please take this notice into consideration before relying on or using any information provided on this website. Any use of the information or content on this website is at the user's own discretion and responsibility.");
+};
 
 let form = document.getElementById("form");
 let input1 = document.getElementById("name");
